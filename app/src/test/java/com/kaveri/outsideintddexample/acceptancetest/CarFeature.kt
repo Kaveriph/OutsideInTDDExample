491a0839dbffcbf61ca80dcfd56676884817d64b
khindi <kaverip.hindi@amadeus.com>
2022-04-27 18:32:12 +0530

    Testing Coroutines

--- app/src/test/java/com/kaveri/outsideintddexample/acceptancetest/CarFeature.kt
+++ app/src/test/java/com/kaveri/outsideintddexample/acceptancetest/CarFeature.kt
@@ -4,6 +4,7 @@ import com.kaveri.outsideintddexample.Car
 import com.kaveri.outsideintddexample.Engine
 import junit.framework.TestCase.assertEquals
 import junit.framework.TestCase.assertTrue
+import kotlinx.coroutines.test.runTest
 import org.junit.Test
 
 class CarFeature {
@@ -12,13 +13,13 @@ class CarFeature {
     val car = Car(6.0, engine)
 
     @Test
-    fun carIsLoosingFuelWhenItTurnsOn() {
+    fun carIsLoosingFuelWhenItTurnsOn() = runTest {
         car.turnOn()
         assertEquals(5.5, car.fuel)
     }
 
     @Test
-    fun carIdTurningOnEngineAndIncreasingTemperature() {
+    fun carIdTurningOnEngineAndIncreasingTemperature() = runTest {
         car.turnOn()
         assertTrue(car.engine.isTurnedOn)
         assertEquals(95.0, car.engine.temperature)
--- app/src/test/java/com/kaveri/outsideintddexample/unittests/CarShould.kt
+++ app/src/test/java/com/kaveri/outsideintddexample/unittests/CarShould.kt
@@ -6,20 +6,23 @@ import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.times
 import com.nhaarman.mockitokotlin2.verify
 import junit.framework.TestCase.assertEquals
+import kotlinx.coroutines.test.runBlockingTest
+import kotlinx.coroutines.test.runTest
 import org.junit.Test
 
 class CarShould {
 
-    val engine :Engine = mock()
+    val engine: Engine = mock()
     private val car: Car = Car(5.0, engine)
+
     @Test
-    fun looseFuelWhenItTurnsOn() {
+    fun looseFuelWhenItTurnsOn() = runTest {
         car.turnOn()
         assertEquals(4.5, car.fuel)
     }
 
     @Test
-    fun turnOnItsEngine() {
+    fun turnOnItsEngine() = runTest {
         car.turnOn()
         verify(engine, times(1)).turnOn()
     }
