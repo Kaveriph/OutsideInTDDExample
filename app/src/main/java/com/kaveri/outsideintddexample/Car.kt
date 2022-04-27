3f785ad6325b79ecea94415de5c823720d423a76
khindi <kaverip.hindi@amadeus.com>
2022-04-27 18:20:58 +0530

    Testing Coroutines

--- app/src/main/java/com/kaveri/outsideintddexample/Car.kt
+++ app/src/main/java/com/kaveri/outsideintddexample/Car.kt
@@ -1,10 +1,15 @@
 package com.kaveri.outsideintddexample
 
+import kotlinx.coroutines.delay
+
 class Car(var fuel: Double,
     val engine: Engine) {
 
-    fun turnOn() {
+    suspend fun turnOn() {
         fuel -= 0.5
+        println("turn on called on ${Thread.currentThread().name}")
+        delay(6000)
+        println("delay returned back to ${Thread.currentThread().name}")
         engine.turnOn()
     }
 
--- app/src/main/java/com/kaveri/outsideintddexample/MainActivity.kt
+++ app/src/main/java/com/kaveri/outsideintddexample/MainActivity.kt
@@ -2,10 +2,17 @@ package com.kaveri.outsideintddexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        var car = Car(200.0, Engine())
+        CoroutineScope(Dispatchers.Main).launch {
+            car.turnOn()
+        }
         setContentView(R.layout.activity_main)
     }
 }
